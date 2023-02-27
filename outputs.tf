5465531a4c940a0bfc777446685ad9d645220429
JFMajer <jakubmajer@live.com>
2023-02-27 18:45:07 +0100

    added first output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,15 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
   description = "value of the vpc id"
 }
+
+# public subnets ids
+output "public_subnets_ids" {
+  value = aws_subnet.public_subnets.*.id
+  description = "value of the public subnets ids"
+}
+
+# private subnets ids
+output "private_subnets_ids" {
+  value = aws_subnet.private_subnets.*.id
+  description = "value of the private subnets ids"
+}
