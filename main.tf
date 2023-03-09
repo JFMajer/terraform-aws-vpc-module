a1cffaf2bfb607bff1ae02887ce497860d63ee88
JFMajer <jakubmajer@live.com>
2023-03-09 21:28:01 +0100

    better comments

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ locals {
   vpc_name = "${var.name_prefix}-vpc"
 }
 
+#######################
+# VPC                 #
+#######################
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
@@ -11,6 +14,9 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+############################
+# Public subnets - tier 1  #
+############################
 resource "aws_subnet" "public_subnets" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.${count.index + 10}.0/24"
@@ -23,6 +29,9 @@ resource "aws_subnet" "public_subnets" {
   }
 }
 
+############################
+# Private subnets - tier 2 #
+############################
 resource "aws_subnet" "private_subnets" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.${count.index + 20}.0/24"
@@ -35,6 +44,24 @@ resource "aws_subnet" "private_subnets" {
   }
 }
 
+#####################################
+# Private subnets - tier 3 database #
+#####################################
+resource "aws_subnet" "private_rds_subnets" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.${count.index + 30}.0/24"
+  availability_zone = element(var.availability_zones, count.index)
+
+  count = var.private_subnets_count
+
+  tags = {
+    Name = "private_rds_10.0.${count.index + 30}.0_${element(var.availability_zones, count.index)}"
+  }
+}
+
+#####################################
+# Internet gateway                  #
+#####################################
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
@@ -43,6 +70,9 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+#####################################
+# Public RT and association         #
+#####################################
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.vpc.id
 
@@ -62,6 +92,9 @@ resource "aws_route_table_association" "public_rt_association" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+#####################################
+# NAT Gateway and it's EIP          #
+#####################################
 resource "aws_eip" "nat" {
   vpc = true
 
@@ -81,6 +114,9 @@ resource "aws_nat_gateway" "nat" {
     depends_on = [aws_internet_gateway.igw]
 }
 
+#####################################
+# Private RT and association        #
+#####################################
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.vpc.id
 
@@ -100,18 +136,10 @@ resource "aws_route_table_association" "private_rt_association" {
   route_table_id = aws_route_table.private_rt.id
 }
 
-resource "aws_subnet" "private_rds_subnets" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.${count.index + 30}.0/24"
-  availability_zone = element(var.availability_zones, count.index)
 
-  count = var.private_subnets_count
-
-  tags = {
-    Name = "private_rds_10.0.${count.index + 30}.0_${element(var.availability_zones, count.index)}"
-  }
-}
-
+#####################################
+# RDS Subnet Group                  #
+#####################################
 resource "aws_db_subnet_group" "rds_mysql" {
   name       = "rds-mysql-${local.vpc_name}"
   subnet_ids = aws_subnet.private_rds_subnets.*.id
