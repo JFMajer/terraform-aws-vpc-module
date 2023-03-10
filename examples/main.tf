56c601efd8cb5ef0e22831b871477b12e69857ef
JFMajer <jakubmajer@live.com>
2023-03-10 21:29:39 +0100

    added gh actions workflow for testing

--- examples/main.tf
+++ examples/main.tf
@@ -8,21 +8,31 @@ terraform {
   }
   
   backend "s3" {
-    bucket = "terraform-state-4632746528"
-    key    = "vpc/terraform.tfstate"
-    region = "eu-north-1"
-    dynamodb_table = "terraform-state"
+    bucket = "#{S3_BUCKET}#"
+    key    = "vpc-test/terraform.tfstate"
+    region = "#{AWS_REGION}#"
+    dynamodb_table = "#{DYNAMO_TABLE}#"
     encrypt = true
   }
 }
 
 provider "aws" {
-  region = "eu-north-1"
+  region = "#{AWS_REGION}#"
+  assume_role {
+    role_arn = "#{AWS_ROLE_TO_ASSUME}#"
+  }
+  default_tags {
+    tags = {
+      Environment = "#{ENV}#"
+      ManagedBy = "terraform"
+    }
+  }
 }
 
 module "vpc" {
     source = "../"
     vpc_cidr = "10.0.0.0/16"
     public_subnets_count = 2
-    private_subnets_count = 2    
+    private_subnets_count = 2
+    name_name_prefix = "#ENV#"    
 }
